e538cd75e74dd8fd98b6370d47985a71d03630d4
Pierre <pi.billaud@gmail.com>
2021-11-17 17:20:52 +0100

    Sound scripts + sounds audio files + prefab sound manager added

---
+++ Assets/Scripts/Sound/SoundManager.cs
@@ -0,0 +1,57 @@
+using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class SoundManager : Singleton<SoundManager>
+{
+
+    public SoundParam[] sounds;
+
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    public void Start()
+    {
+        Play("main_theme", this.gameObject);
+    }
+
+
+    public void Play(string name, GameObject sourceObject)
+    {
+        try
+        {
+            SoundParam sp = Array.Find(sounds, sound => sound.name.Equals(name));
+
+
+            //Si vide, on l'initialise (il va avoir la position de l'objet child)
+            if (sp.audioSource == null)
+            {
+
+                sp.audioSource = sourceObject.AddComponent<AudioSource>();
+
+                sp.audioSource.clip = sp.audioClip;
+                sp.audioSource.volume = sp.volume;
+
+                sp.audioSource.pitch = sp.pitch;
+                sp.audioSource.loop = sp.loop;
+
+                sp.audioSource.spatialBlend = sp.spatialBlend;
+
+            }
+
+            if (sp.loop == true)
+            {
+                sp.audioSource.Play();
+            }
+            {
+                sp.audioSource.PlayOneShot(sp.audioClip);
+            }
+        }
+        catch (NullReferenceException)
+        {
+            Debug.Log("Audio clip" + name + " non existant");
+        }
+    }
+}
