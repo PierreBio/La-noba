6d9ef376a322257a1985469718e1a309362d01f3
Pierre <pi.billaud@gmail.com>
2021-11-17 09:58:13 +0100

    initial commit

---
+++ Assets/Scripts/ClickableText.cs
@@ -0,0 +1,191 @@
+using TMPro;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ClickableText : MonoBehaviour, IPointerClickHandler
+{
+    public Camera m_Camera;
+
+    private Node[] storyNodes;
+
+    private Node currentNode;
+
+    private IDictionary<string, string> currentVariables = new Dictionary<string, string>();
+
+    public void Awake()
+    {
+        storyNodes = ImportTwison._instance.storyNodes.passages;
+
+        currentNode = ImportTwison._instance.storyNodes.passages[0];
+
+        currentVariables = ImportTwison._instance.variableDictionnary;
+
+        this.displayCurrentNode();
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        var text = gameObject.GetComponent<TextMeshProUGUI>();
+
+        if(eventData.button == PointerEventData.InputButton.Left)
+        {
+            int linkIndex = TMP_TextUtilities.FindIntersectingLink(text, Input.mousePosition, m_Camera);
+
+            if (linkIndex > -1)
+            {
+                var linkInfo = text.textInfo.linkInfo[linkIndex];
+                var linkId = linkInfo.GetLinkID();
+
+                changeCurrentNode(linkId);
+            }
+        }
+    }
+
+    private void changeCurrentNode(string newNodeName)
+    {
+        int indexNewNodeName = 0;
+
+        foreach(Node node in storyNodes)
+        {
+            if(node.name == newNodeName)
+            {
+                indexNewNodeName = node.pid - 1;
+                break;
+            }
+        }
+
+        //NORMAL COPY DOESNT WORK BECAUSE NODE IS CHANGED BUT NOT REINITIALISED
+        //currentNode = storyNodes[indexNewNodeName];
+
+        //WE HAVE TO MAKE DEEP COPY TO PASS A VARIABLE BY COPY AND NOT BY REFERENCE TO NOT ERASE IT
+        Node existingNode = ImportTwison._instance.storyNodes.passages[indexNewNodeName];
+        currentNode = DeepCopy.DeepCopyNode(existingNode);
+
+        displayCurrentNode();
+    }
+
+    private void displayCurrentNode()
+    {
+        Debug.Log(currentNode.text);
+        //AVANT LE DISPLAY ON DOIT DISSIMULER CERTAINES VALEURS (LES DECLARATIONS DE CERTAINES VARIABLES) 
+
+        //DURANT LE CHANGEMENT DE NOEUD ON VERIFIE SI ON DOIT CHANGER LA VALEUR DE CERTAINES VARIABLES 
+
+        //NOTAMMENT POUR EMPÊCHER OU PERMETTRE L'AFFICHAGE DE CERTAINS EMBRANCHEMENTS
+
+        List<int> listIndexVariableDefinitions = HandleString.AllIndexesOf(currentNode.text, "(set: $");
+        List<int> listEndParenthesis = HandleString.AllIndexesOf(currentNode.text, ")");
+
+        if (listIndexVariableDefinitions != null)
+        {
+            for (var i = 0; i < listIndexVariableDefinitions.Count; i++)
+            {
+                int indexEndParenthesis = 0;
+
+                for (var j = 0; j < listEndParenthesis.Count; j++)
+                {
+                    if (listEndParenthesis[j] > listIndexVariableDefinitions[i])
+                    {
+                        indexEndParenthesis = listEndParenthesis[j];
+                        break;
+                    }
+                }
+
+                string contentVariable = currentNode.text.Substring(listIndexVariableDefinitions[i], indexEndParenthesis + 1);
+
+                string variableName = HandleString.getBetween(contentVariable, "(set: $", " to");
+                string variableValue = HandleString.getBetween(contentVariable, "to \"", "\")");
+
+                if (currentVariables.ContainsKey(variableName)) //ON ATTRIBUT LA NOUVELLE VALEUR A NOTRE VARIABLE
+                {
+                    currentVariables[variableName] = variableValue;
+                }
+
+                //ON ENLEVE L'AFFICHAGE DE LA DECLARATION / ASSIGNATION DES VARIABLES
+                currentNode.text = currentNode.text.Replace(contentVariable, "");
+            }
+        }
+
+        //GET ALL IFS
+        List<int> listIndexIfConditions = HandleString.AllIndexesOf(currentNode.text, "(if: $");
+        List<int> listEndifParenthesis = HandleString.AllIndexesOf(currentNode.text, ")");
+        List<int> listIndexElseConditions = HandleString.AllIndexesOf(currentNode.text, "(else:)");
+
+        if (listIndexIfConditions != null)
+        {
+            for (var i = 0; i < listIndexIfConditions.Count; i++)
+            {
+                Debug.Log(i);
+                Debug.Log(currentNode.text);
+                if(i < HandleString.AllIndexesOf(currentNode.text, "(if: $").Count)
+                {
+                    verifyCondition(listIndexIfConditions, listEndifParenthesis, listIndexElseConditions, i);
+                }
+            }
+        }
+
+        GetComponent<TMPro.TextMeshProUGUI>().text = currentNode.text;
+    }
+
+    private int verifyCondition(List<int> listIndexIfConditions, List<int>  listEndifParenthesis, List<int> listIndexElseConditions, int index, int loop = 0)
+    {
+        int indexEndifParenthesis = 0;
+
+        for (var j = 0; j < listEndifParenthesis.Count; j++)
+        {
+            if (listEndifParenthesis[j] > listIndexIfConditions[index])
+            {
+                indexEndifParenthesis = listEndifParenthesis[j];
+                break;
+            }
+        }
+        Debug.Log(listIndexIfConditions[index]);
+        Debug.Log(System.Math.Abs((indexEndifParenthesis + 1) - listIndexIfConditions[index]));
+
+        string contentVariable = currentNode.text.Substring(listIndexIfConditions[index], System.Math.Abs((indexEndifParenthesis + 1) - listIndexIfConditions[index]));
+        string variableName = HandleString.getBetween(contentVariable, "(if: $", " is");
+        string variablePotentialValue = HandleString.getBetween(contentVariable, "is \"", "\")");
+
+        string content = HandleString.getBetween(currentNode.text, contentVariable + "[", " ]");
+        if (currentVariables.ContainsKey(variableName))
+        {
+            if (currentVariables[variableName] == variablePotentialValue) //ON CHECK SI LA CONDITION EST VERIFIEE ET SI OUI ON DISPLAY LE TEXTE SITUE DANS LE THEN
+            {
+                if (content.IndexOf("(if: $") != -1)
+                {
+                    verifyCondition(listIndexIfConditions, listEndifParenthesis, listIndexElseConditions, index + 1, loop + 1);
+                    listIndexIfConditions.RemoveAt(index + 1);
+                }
+
+                //On réactualise les nouvelles positions
+                contentVariable = currentNode.text.Substring(listIndexIfConditions[index], System.Math.Abs((indexEndifParenthesis + 1) - listIndexIfConditions[index]));
+                variableName = HandleString.getBetween(contentVariable, "(if: $", " is");
+                variablePotentialValue = HandleString.getBetween(contentVariable, "is \"", "\")");
+
+                content = HandleString.getBetween(currentNode.text, contentVariable + "[", " ]");
+
+                int indexForContentToDelete = listIndexIfConditions[index] + contentVariable.Length + 1 + content.Length + 1;
+                currentNode.text = currentNode.text.Remove(indexForContentToDelete, 1);
+                currentNode.text = currentNode.text.Replace(contentVariable + "[", "");
+
+                return loop;
+            }
+            else
+            {
+                string finalContentToDelete = currentNode.text.Substring(listIndexIfConditions[index], contentVariable.Length + 1 + content.Length + 2);
+                currentNode.text = currentNode.text.Replace(finalContentToDelete, "");
+
+                while((currentNode.text.IndexOf("(if: $") > currentNode.text.IndexOf(" ]")) || (currentNode.text.IndexOf("(if: $") == -1 && currentNode.text.IndexOf(" ]") != -1))
+                {
+                    currentNode.text = HandleString.ReplaceFirst(currentNode.text, " ]", "");
+                }
+
+                return loop;
+            }
+        }
+
+        return 0;
+    }
+}
---
+++ Assets/Scripts/ImportTwison.cs
@@ -0,0 +1,224 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImportTwison : MonoBehaviour
+{
+    public TextAsset jsonFile;
+
+    public static ImportTwison _instance;
+
+    public Nodes storyNodes;
+
+    public IDictionary<string, string> variableDictionnary = new Dictionary<string, string>();
+
+    void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(this.gameObject);
+
+            //Rest of your Awake code
+            storyNodes = JsonUtility.FromJson<Nodes>(jsonFile.text);
+
+            foreach (Node node in ImportTwison._instance.storyNodes.passages)
+            {
+                if (node.links != null)
+                {
+                    List<int> listIndexVariableDefinitions = HandleString.AllIndexesOf(node.text, "(set: $");
+                    List<int> listEndParenthesis = HandleString.AllIndexesOf(node.text, ")");
+
+                    if(listIndexVariableDefinitions != null)
+                    {
+                        for (var i = 0; i < listIndexVariableDefinitions.Count; i++)
+                        {
+                            int indexEndParenthesis = 0;
+
+                            for (var j = 0; j < listEndParenthesis.Count; j++)
+                            {
+                                if(listEndParenthesis[j] > listIndexVariableDefinitions[i])
+                                {
+                                    indexEndParenthesis = listEndParenthesis[j];
+                                    break;
+                                }
+                            }
+
+                            string contentVariable = node.text.Substring(listIndexVariableDefinitions[i], indexEndParenthesis + 1);
+
+                            string variableName = HandleString.getBetween(contentVariable, "(set: $", " to");
+                            string variableValue = HandleString.getBetween(contentVariable, "to \"", "\")");
+
+                            if (!variableDictionnary.ContainsKey(variableName)) //SI LE DICTIONNAIRE N'A PAS ENCORE ENREGISTRE CETTE VARIABLE ALORS ON L'ENREGISTRE
+                            {
+                                variableDictionnary.Add(new KeyValuePair<string, string>(variableName, variableValue));
+                            }
+
+                            //ENLEVE L'AFFICHAGE DE LA DECLARATION DES VARIABLES
+                            //node.text = node.text.Replace(contentVariable, "");
+                        }
+                    }
+
+                    //PARCOURT TOUS LES STRING ENTRE // POUR METTRE DE L'ITALIQUE
+                    /*List<int> listItalicTagIndexes = HandleString.AllIndexesOf(node.text, "//");
+
+                    if (listItalicTagIndexes != null)
+                    {
+                        for (var i = 0; i < listItalicTagIndexes.Count; i+=2)
+                        {
+                            string toReplace = HandleString.getBetweenIndexes(
+                                node.text, 
+                                listItalicTagIndexes[i], 
+                                listItalicTagIndexes[i+1] + 2
+                                );
+
+                            string replacement = "<i>" + HandleString.getBetweenIndexes(
+                                node.text, 
+                                listItalicTagIndexes[i] + 2 + (i/2 * 3),
+                                listItalicTagIndexes[i + 1] + (i/2 * 3)
+                                ) + "</i>";
+
+                            Debug.Log(replacement);
+
+                            node.text = node.text.Replace(toReplace, replacement);
+                        }
+                    }*/
+
+                    //PARCOURS TOUS LES STRING ENTRE [[ ]] POUR MODIFIER LE FORMAT ET METTRE DE LA COULEUR + UN LIEN
+                    List<int> listIndexStart = HandleString.AllIndexesOf(node.text, "[[");
+                    List<int> listIndexEnd = HandleString.AllIndexesOf(node.text, "]]");
+
+                    List<string> listRawTextClickable = new List<string>();
+                    List<string> listLinkClickable = new List<string>();
+                    List<string> listTextClickable = new List<string>();
+
+                    if (listIndexStart != null)
+                    {
+                        for (var i = 0; i < listIndexStart.Count; i++)
+                        {
+                            string rawOccurence = HandleString.getBetweenIndexes(node.text, listIndexStart[i], listIndexEnd[i] + 2);
+                            string linkOccurence = HandleString.getBetweenIndexes(node.text, listIndexStart[i] + 2, listIndexEnd[i]);
+                            string occurence = HandleString.getBetweenIndexes(node.text, listIndexStart[i] + 2, listIndexEnd[i]);
+
+                            if(occurence.IndexOf("->") != -1)
+                            {
+                                linkOccurence = HandleString.getAfter(occurence, "->");
+                                occurence = occurence.Substring(0, occurence.IndexOf("->"));
+                            }
+
+                            listRawTextClickable.Add(rawOccurence);
+                            listLinkClickable.Add(linkOccurence);
+                            listTextClickable.Add(occurence);
+                        }
+                    }
+
+                    for (var i = 0; i < listRawTextClickable.Count; i++)
+                    {
+                        node.text = node.text.Replace(
+                            listRawTextClickable[i],
+                            "<link=" + listLinkClickable[i] + "><b><color=blue>" + listTextClickable[i] + "</color></b></link>"
+                            );
+                    }
+
+                    //PARCOURT TOUS LES LIENS
+                    /*foreach (Link link in node.links)
+                    {
+                        if (link != null)
+                        {
+                            Debug.Log(link.name);
+                        }
+                    }*/
+                }
+            }
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+}
+
+[System.Serializable]
+public class Link
+{
+    public int pid;
+    public string name;
+    public string link;
+}
+
+[System.Serializable]
+public class Node
+{
+    public int pid;
+    public string text;
+    public Link[] links;
+    public string name;
+}
+
+[System.Serializable]
+public class Nodes
+{
+    public Node[] passages;
+}
+
+public static class DeepCopy
+{
+    public static Node DeepCopyNode(Node n)
+    {
+        Node temp = new Node();
+        temp.pid = n.pid;
+        temp.text = n.text;
+        temp.links = n.links;
+        temp.name = n.name;
+
+        return temp;
+    }
+}
+
+public static class HandleString
+{
+    public static List<int> AllIndexesOf(this string str, string value)
+    {
+        List<int> indexes = new List<int>();
+        for (int index = 0; ; index += value.Length)
+        {
+            index = str.IndexOf(value, index);
+            if (index == -1)
+                return indexes;
+            indexes.Add(index);
+        }
+    }
+
+    public static string getAfter(string strSource, string strBeforeAfter)
+    {
+        return strSource.Substring(strSource.LastIndexOf(strBeforeAfter) + strBeforeAfter.Length);
+    }
+
+    public static string getBetween(string strSource, string strStart, string strEnd)
+    {
+        if (strSource.Contains(strStart) && strSource.Contains(strEnd))
+        {
+            int Start, End;
+            Start = strSource.IndexOf(strStart, 0) + strStart.Length;
+            End = strSource.IndexOf(strEnd, Start);
+            return strSource.Substring(Start, End - Start);
+        }
+
+        return "";
+    }
+
+    public static string getBetweenIndexes(string strSource, int indexStart, int indexEnd)
+    {
+        return strSource.Substring(indexStart, System.Math.Abs(indexStart - indexEnd));
+    }
+
+    public static string ReplaceFirst(string text, string search, string replace)
+    {
+        int pos = text.IndexOf(search);
+        if (pos < 0)
+        {
+            return text;
+        }
+        return text.Substring(0, pos) + replace + text.Substring(pos + search.Length);
+    }
+}
