b8852fb11ab42b504febff73deb667163d9c297d
Hichame <hichame.ouk@gmail.com>
2022-08-31 12:11:38 +0200

    Add cursor change when you hover clickable text

---
+++ Assets/Scripts/CursorScript.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorScript : MonoBehaviour
+{
+    public enum CursorType
+    {
+        ARROW,
+        HOVER
+    }
+
+    public Texture2D cursorArrow;
+    public Texture2D cursorHover;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //set the cursor origin to its centre. (default is upper left corner)
+        Vector2 cursorOffset = Vector2.zero;
+
+        //Sets the cursor to the Crosshair sprite with given offset 
+        //and automatic switching to hardware default if necessary
+        Cursor.SetCursor(cursorArrow, cursorOffset, CursorMode.Auto);
+    }
+
+    public void setCursor(CursorType _type)
+    {
+        switch (_type)
+        {
+            case CursorType.ARROW:
+                Cursor.SetCursor(cursorArrow, Vector2.zero, CursorMode.Auto);
+                break;
+            case CursorType.HOVER:
+                Cursor.SetCursor(cursorHover, Vector2.zero, CursorMode.Auto);
+                break;
+            default:
+                break;
+        }
+    }
+}
